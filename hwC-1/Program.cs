d1f8dcee509614115930a1efdef6af3f3184769a
unknown <Anatolii>
2022-12-15 00:07:33 +0300

    add folders

---
+++ hwC-1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите пятизначное число ");
+        string? N = Console.ReadLine();
+
+        int Num = Convert.ToInt32(N);
+        int Num1 = Num / 10000;
+        int Num5 = Num % 10;
+        int Num2 = Num / 1000 - Num1 * 10;
+        int Num3 = Num % 100;
+        int Num4 = (Num3 - Num1) / 10;
+
+        if (Num1 == Num5 & Num2 == Num4)
+            Console.WriteLine("Число полином");
+        else
+            Console.WriteLine("Число {Num} НЕ полином");
+    }
+}                                                    
---
+++ hwC-2/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 21
+
+//Напишите программу, которая принимает на вход координаты двух 
+//точек и находит расстояние между ними в 3D пространстве.
+
+//A (3,6,8); B (2,1,-7), -> 15.84
+
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите X1  ");
+int Xa = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y1  ");
+int Ya = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Z1  ");
+int Za = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите X2  ");
+int Xb = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Y2 ");
+int Yb = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите Z2  ");
+int Zb = int.Parse(Console.ReadLine());
+
+double Xab = (Xb - Xa);
+double Yab = (Yb - Ya);
+double Zab = (Zb - Za);
+
+double XYZab = Math.Pow(Xab, 2) + Math.Pow(Yab, 2) + Math.Pow(Zab, 2);
+
+double result = Math.Sqrt(XYZab);
+
+Console.WriteLine($"расстояние  в 3D пространстве {result}");
+
+
+
